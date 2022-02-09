0f8f104e33503e9056ac8b94025d431b0428a8a8
M4teusPrado <mateus.silvaprado@gmail.com>
2022-02-09 16:23:38 -0300

    Resuolução Exercicio 2 - Acertar n° aleatório

--- src/br/com/GFT/Ex01/Exercicio01.java
+++ src/br/com/GFT/Ex01/Exercicio01.java
@@ -3,5 +3,7 @@ package br.com.GFT.Ex01;
 public class Exercicio01 {
 
     public static void main(String[] args) {
+
+
     }
 }
--- src/br/com/GFT/Ex02/NumeroAleatório.java
+++ src/br/com/GFT/Ex02/NumeroAleatório.java
@@ -1,2 +1,32 @@
-package br.com.GFT.Ex02;public class NumeroAleatório {
+package br.com.GFT.Ex02;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumeroAleatório {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = random.nextInt(11),  numeroUsuario = 0;
+
+        while(true)  {
+
+            // Recebe valor do usuario
+            System.out.printf("Digite um numero \n");
+            numeroUsuario = Integer.parseInt(scanner.nextLine());
+
+            // Verifica se o numero input do usuario é igual ao numero gerado
+                // True = Quebra o laço de repetição
+                // False = O usuario recebe a mensagem, solicitando uma nova tentativa
+            if(numero == numeroUsuario) break;
+            else System.out.printf("Tente outro numero!\n");
+        }
+
+        System.out.printf("voce acertou!!\n");
+        scanner.close();
+    }
+
 }
